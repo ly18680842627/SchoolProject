ec636a832821222ccf836a71c512696c2dbe787f
刘杨 <740400496@qq.com>
2020-12-02 14:43:04 +0800

    #测试提交数据初始化项目#

---
+++ DatainitPerject/src/main/java/com/llz/demo/DatainitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.llz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatainitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatainitProjectApplication.class, args);
+	}
+
+}
